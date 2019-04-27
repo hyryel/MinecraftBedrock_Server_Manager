902263c402440e013319916e23af5a30a90745af
Hyryel <hyryel@hotmail.com>
2019-04-27 23:54:43 +0200

    First commit

---
+++ Minecraft Bedrock server Wrapper/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minecraft_Bedrock_server_Wrapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private ServerManager _server;
+
+        private void MnuQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+        private void MnuStopServer_Click(object sender, RoutedEventArgs e)
+        {
+            _server.SendCommand("stop");
+        }
+
+        System.Collections.ObjectModel.ObservableCollection<string> _infos = new System.Collections.ObjectModel.ObservableCollection<string>();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstInfo.DataContext = _infos;
+            _server = new ServerManager("G:\\jeux\\Minecraft bedrock serveur\\bedrock_server.exe");
+            _server.StateUpdated += _server_StateUpdated;
+            _server.Start();
+        }
+
+        private void _server_StateUpdated(object sender,ServerUpdatedEventArgs e)
+        {
+            _infos.Add(e.Message);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ Minecraft Bedrock server Wrapper/ServerManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Minecraft_Bedrock_server_Wrapper
+{
+    public enum MessageType
+    {
+        Error,
+        Info,
+
+    }
+    internal class ServerUpdatedEventArgs : EventArgs
+    {
+        public MessageType MessageType { get; set; }
+        public string Message { get; set; }
+    }
+    internal class DataReceivedInfo
+    {
+        public string Data { get; set; }
+        public MessageType MsgType { get; set; }
+    }
+    internal class ServerManager : IDisposable
+    {
+        public string ExecutablePath { get; set; }
+
+        private StreamWriter _inputWriter;
+        private Process _server;
+
+        private SynchronizationContext _context;
+        public ServerManager(string executablePath)
+        {
+            _context = SynchronizationContext.Current ?? new SynchronizationContext();
+            ExecutablePath = executablePath;
+        }
+
+        public void Start()
+        {
+            _server = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo si = new System.Diagnostics.ProcessStartInfo(ExecutablePath)
+            {
+                CreateNoWindow = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+
+            _server.StartInfo = si;
+            _server.ErrorDataReceived += _server_ErrorDataReceived;
+            _server.OutputDataReceived += _server_OutputDataReceived;
+
+            _server.Start();
+            _inputWriter = _server.StandardInput;
+            _server.BeginOutputReadLine();
+            _server.BeginErrorReadLine();
+        }
+
+        public event EventHandler<ServerUpdatedEventArgs> StateUpdated;
+
+        private void MySendOrPostCallback(object state)
+        {
+            DataReceivedInfo data = (DataReceivedInfo) state;
+            StateUpdated?.Invoke(this, new ServerUpdatedEventArgs { Message = data.Data, MessageType = data.MsgType } );
+        }
+
+        private void _server_ErrorDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            _context.Post(MySendOrPostCallback, new DataReceivedInfo { Data = e.Data, MsgType = MessageType.Error });
+        }
+        private void _server_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            _context.Post(MySendOrPostCallback, new DataReceivedInfo { Data = e.Data, MsgType = MessageType.Info });
+        }
+
+        public void SendCommand(string command)
+        {
+            _inputWriter.WriteLine(command);
+        }
+        ~ServerManager()
+        {
+            Dispose();
+        }
+        private bool _disposed = false;
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                if (_server != null)
+                {
+                    _server.CancelErrorRead();
+                    _server.CancelOutputRead();
+                    if (!_server.HasExited)
+                    {
+                        _server.Kill();
+                    }
+                }
+            }
+        }
+    }
+}
